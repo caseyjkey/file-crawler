c4ad9ae1ca736d62d99386802e27e280c52584c9
Casey Key <caseykey@juneau.cs.colostate.edu>
2019-03-05 18:33:10 -0700

    Converted Path to Bunch

---
+++ HW4/Bunch.cc
@@ -0,0 +1,198 @@
+#include "Bunch.h"
+
+#include <iostream>           // For cerr
+#include <sys/types.h>
+#include <fstream>            // For ifstream
+#include <sstream>
+#include <vector>
+#include <sys/stat.h>
+#include <pwd.h>
+#include <grp.h>
+using namespace std;
+
+// Constructor
+Bunch::Bunch(string path, string magic) {
+            // Open a statbuf
+            struct stat statbuf;
+            int openFile = lstat(path.c_str(), &statbuf);
+            if(openFile != 0) {
+                cerr << PROGNAME << ": cannot access '" << path << "': No such file or directory\n";
+                isNull(true);
+                return;
+            }
+            
+            // Begin assigning values to attributes
+            size_        = statbuf.st_size;
+            mediaTypes   = readMediaTypeFile(magic);
+            path_        = path;
+            access_time_ = time(statbuf, 1, 0, 0);
+            mod_time_    = time(statbuf, 0, 1, 0);
+            status_time_ = time(statbuf, 0, 0 ,1);
+            user_UID_    = user_UID(statbuf);
+            group_UID_   = group_UID(statbuf);
+            user_NAME_   = user_NAME(user_UID_);
+            group_NAME_  = group_NAME(group_UID_);
+            magic_num_   = readMagicNumber(path_);
+            type_        = findMediaType(magic_num_, mediaTypes, statbuf);
+             //sizePath(statbuf);
+
+            permissions(statbuf, permissions_);
+}
+
+string Bunch::PROGNAME = "hw3";
+
+Bunch &Bunch::addEntry(string path, string magic) {
+    Bunch retVal(path, magic);
+    //cout << "adding " << retVal.path_ << "\n";
+    entries.push_back(retVal);
+    //cout << "\nentries size: " << entries.size() << "\n";
+    return this->entries.back();
+}
+
+vector< pair<string, string> > Bunch::readMediaTypeFile(string dir) {
+    ifstream inFile;
+    inFile.open(dir);
+    string token;
+    pair<string, string> mediaEntry;
+    while(inFile >> token) {
+        mediaEntry.first = token;
+        inFile >> token;
+        mediaEntry.second = token;
+        mediaTypes.push_back(mediaEntry);
+    }
+    return mediaTypes;
+}
+
+void Bunch::isNull(bool setTo) {
+    isNull_ = setTo;
+}
+
+int Bunch::user_UID(struct stat & statbuf) {
+    // https://ibm.co/2GwdIIR
+    // Option to access via stat
+    return static_cast<int>(statbuf.st_uid);
+}
+string Bunch::user_NAME(int uid) {
+    struct passwd *pwd;
+    if((pwd = getpwuid(uid)) != NULL)
+        return pwd->pw_name;
+    else
+        cerr << PROGNAME << ": " << uid << " not found in user database\n";
+    return "uid not found";
+}
+int Bunch::group_UID(struct stat &statbuf) {
+    return statbuf.st_gid;
+}
+string Bunch::group_NAME(int gid) {
+    struct group *grp;
+    if((grp = getgrgid(gid)) != NULL)
+        return grp->gr_name;
+    else
+        cerr << PROGNAME << ": " << gid << " not found in group database\n";
+    return "gid not found";
+}
+int Bunch::permissions(struct stat &statbuf, string &output) {
+    ostringstream os;
+    if(S_ISDIR(statbuf.st_mode)) os <<  "d";
+    else if(S_ISLNK(statbuf.st_mode)) os << "l";
+    else if(S_ISREG(statbuf.st_mode)) os << "-";
+    else {
+        cerr << PROGNAME << ": " << path_ << " is an undefined file type, wtf?!\n";
+        return 1;
+    }
+    os << (statbuf.st_mode & S_IRUSR ? 'r' : '-');
+    os << (statbuf.st_mode & S_IWUSR ? 'w' : '-');
+    os << (statbuf.st_mode & S_IXUSR ? 'x' : '-');
+
+    os << (statbuf.st_mode & S_IRGRP ? 'r' : '-');
+    os << (statbuf.st_mode & S_IWGRP ? 'w' : '-');
+    os << (statbuf.st_mode & S_IXGRP ? 'x' : '-');
+
+    os << (statbuf.st_mode & S_IROTH ? 'r' : '-');
+    os << (statbuf.st_mode & S_IWOTH ? 'w' : '-');
+    os << (statbuf.st_mode & S_IXOTH ? 'x' : '-');
+    output = os.str();
+    return 0;
+}
+int Bunch::sizePath(struct stat &statbuf) {
+    return statbuf.st_size;
+}
+string Bunch::time(struct stat &statbuf, bool access = 0, bool mod = 1, bool status = 0) {
+    time_t fileTime;
+    if(mod) {
+        fileTime = statbuf.st_mtime;
+    }
+    else if(access) {
+        fileTime = statbuf.st_atime;
+    }
+    else if(status) {
+        fileTime = statbuf.st_ctime;
+    }
+    auto timeval = localtime(&fileTime);
+    char buf[32];
+    strftime(buf, sizeof(buf), "%Y-%m-%dT%T", timeval);
+    string timeOutput(buf);
+    return timeOutput;
+}
+
+string Bunch::inttohex(int num) {
+    // https://bit.ly/2InTEd9
+    
+    //if(num == 127) num = 137;
+    //cout << "Base 16 num: " << num << " ";
+    string d = "0123456789abcdef"; //
+    string res;
+    while(num > 0) {
+        res = d[num % 16] + res;
+        num /= 16;
+    }
+    if(res.length() == 1) res = "0" + res;
+    //cout << "Decimal result: " << res << "\n";
+    return res;
+}
+
+string Bunch::readMagicNumber(string dir) {
+    ifstream file(dir);
+    char chrctr;
+    string magicNum;
+    for ( int i = 0; i < 32; i++ ) {
+        file.get(chrctr);
+        //if(dir == "/s/bach/a/class/cs253/pub/tree/alpha/iota/kappa")
+            //cout << "get: " << char(chrctr) << "\n";
+        //cout << "character: " << chrctr << "\n";
+        
+        if(chrctr < '!' || chrctr > '~') {
+            //cout << "Inside readMagicNumber if, get read: " << char(chrctr) << "\n";
+            magicNum += "%" +  inttohex(chrctr);
+        } else {
+            magicNum += chrctr;
+            //cout << "Inside readMagicNumber else, get read: " << chrctr << "\n";
+        }
+    }
+    //cout << "magicNum: " << magicNum << "\n";
+    return magicNum;
+}
+
+string Bunch::findMediaType(string magicNum, vector< pair<string, string> > MediaTypes, struct stat &statbuf) {
+    if(S_ISDIR(statbuf.st_mode)) return "inode/directory";
+    if(S_ISLNK(statbuf.st_mode)) return "inode/symlink";
+    if(sizePath(statbuf) == 0) return "inode/empty";
+    bool match;
+    for(auto& elem : MediaTypes) {
+        match = 1;
+        for(unsigned int i = 0; i < elem.first.length(); i++) {
+                    //cout << magicNum[i] << " compared to " << elem.first[i] << "\n";
+            if(magicNum[i] != elem.first[i]) {
+                    match = 0;
+            }
+        }
+        if(match) return elem.second;
+    }
+    return "application/octet-data";
+}
+
+
+
+
+
+
---
+++ HW4/hw4.cc
@@ -0,0 +1,186 @@
+#include "Bunch.h"
+#include <unistd.h>     // For getopt() and getcwd
+#include <dirent.h>     // For readDir()
+#include <iostream>
+#include <string>
+#include <sstream>      // For ostringstream
+using namespace std;
+
+ //corn fritters, georgia
+
+void processFormatString(string tokens, Bunch currentPath) {
+    for(string::size_type i = 0; i < tokens.size(); ++i) {
+        if( tokens[i] != '%') {
+                    cout << tokens[i];
+                }
+        else {
+            ++i;
+            if(tokens[i] == 'n') {
+                cout << currentPath.path_;
+            }
+            else if(tokens[i] == 'p') {
+                cout << currentPath.permissions_;
+            }
+            else if(tokens[i] == 'u') {
+                cout << currentPath.user_UID_;
+            }
+            else if(tokens[i] == 'U') {
+                cout << currentPath.user_NAME_;
+            }
+            else if(tokens[i] == 'g') {
+                cout << currentPath.group_UID_;
+            }
+            else if(tokens[i] == 'G') {
+                cout << currentPath.group_NAME_;
+            }
+            else if(tokens[i] == 's') {
+                cout << currentPath.size_;
+            }
+            else if(tokens[i] == 'a') {
+                cout << currentPath.access_time_;
+            }
+            else if(tokens[i] == 'm') {
+                cout << currentPath.mod_time_;
+            }
+            else if(tokens[i] == 'c') {
+                cout << currentPath.status_time_;
+            }
+            else if(tokens[i] == 'M') {
+                cout << currentPath.type_;
+            }
+        }
+    }
+}
+
+
+Bunch traverse(Bunch path, string magicDir, string format, int aFind) {
+    DIR *dir;
+    struct dirent *entry;
+    struct stat info;
+    
+    ostringstream nextFn;
+    if(path.type_ == "inode/directory") {
+        if((dir = opendir(path.path_.c_str())) == NULL)
+            cerr << Bunch::PROGNAME << ": " << path.path_ << " opendir() error\n";
+        else {
+            while((entry = readdir(dir)) != NULL) {
+                nextFn.str("");
+                nextFn.clear();
+                if( aFind == 1) {
+                    if (*entry -> d_name != '.') { 
+                    cout << "\n" << entry -> d_name;
+                    nextFn << path.path_ << "/" << entry->d_name;
+                    Bunch newEntry = path.addEntry(nextFn.str(), magicDir);
+                    cout << "\n";
+                    processFormatString(format, newEntry);
+                    if (stat(nextFn.str().c_str(), &info) != 0) 
+                        cerr << Bunch::PROGNAME << ": stat(" << nextFn.str() << ") error\n";
+                    else if (S_ISDIR(info.st_mode))
+                        traverse(Bunch(nextFn.str(), magicDir), magicDir, format, aFind);
+                    }
+                }
+                else {
+                    if ((entry -> d_name[0]) != '.') {
+ 
+                    nextFn << path.path_ << "/" << entry->d_name;
+                    
+                    Bunch newEntry = path.addEntry(nextFn.str(), magicDir);
+                    cout << "\n";
+                    processFormatString(format, newEntry);
+                    if (stat(nextFn.str().c_str(), &info) != 0) 
+                        cerr << Bunch::PROGNAME << ": stat(" << nextFn.str() << ") error\n";
+                    else if (S_ISDIR(info.st_mode))
+                        traverse(Bunch(nextFn.str(), magicDir), magicDir, format, aFind);
+                    }
+                }
+            }
+            closedir(dir);
+        }
+    }
+    return path;
+}
+
+static string PROGNAME;
+
+int main(int argc, char* argv[]) {
+    PROGNAME = argv[0];
+    Bunch::PROGNAME = static_cast<string>(PROGNAME);
+	if(argv[1] == nullptr) {
+		cerr << PROGNAME << " usage: ./hw3 [FILE] [DIRECTORY]\n";
+		return 1;
+	}
+
+	// ------------------- Default magic-file ------------------------------ 
+	const string csDir = getpwnam("cs253") -> pw_dir; // reads the directory of cs253 user
+    string dir = csDir + "/pub/media-types";
+    // ---------------------------------------------------------------------
+    
+	// ---------------- Parse command line options -------------------------
+	int opt;
+	int aFind, mFind, fFind;
+	string magicFile, format;
+
+    aFind = 0;
+    mFind = 0;
+    fFind = 0;
+	while((opt = getopt(argc, argv, "am:f:")) != -1) {
+        switch (opt) {
+            case 'm':
+                if(mFind == 1) {
+                    cerr << PROGNAME << ": error media-type already defined\n";
+                    return 1;
+                }
+                else {
+                    mFind = 1;
+                    dir = optarg;
+                    // Check if valid
+                    struct stat buf;
+                    if(stat(dir.c_str(), &buf) != 0) {
+                        cerr << PROGNAME << ": error '" << dir << "' can't be opened\n";
+                        return 1;
+                    }
+                }
+                break;
+            case 'a':
+                aFind = 1;
+                break;
+            case 'f':
+                if(fFind == 1) {
+                    cerr << PROGNAME << ": error, format already defined\n";
+                    return 1;
+                }
+                else {
+                    fFind = 1;
+                    format = optarg;
+                }
+                break;
+            default:
+                cerr << "Usage: " << PROGNAME << " [-a] [-m magic-file] [-f format] path" << endl;
+                return 1;
+        }
+    }
+    if(fFind == 0) format = "%p %U %G %s %n";
+    // --------------------------------------------------------------------
+    
+    // ------------------ End Parse Command Line Arguments ----------------
+    
+    // Create a vector containing each path passed as an argument
+	vector<Bunch> paths;
+	for(int i = optind; i < argc; i++) {
+        string str(argv[i]);
+		Bunch path(str, dir);
+		paths.push_back(path);
+	}
+    
+    // --------------------------------------------------------------------
+   
+    // ----------------- Traverse and Output Bunchs ------------------------
+	for(auto path : paths) {
+        Bunch currentPath(path);
+        if(currentPath.isNull_) continue;
+        processFormatString(format, path);
+        path = traverse(path, dir, format, aFind);
+        cout << endl;
+    }
+    return 0;
+}
